748e7738a4738088d470c267a52106473d3357ef
dhiogoboza <dhiogoboza@gmail.com>
2020-06-10 08:23:38 -0300

    Persist texts

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
-function enableAll(selector) {
-    var clipboard = new Clipboard(selector);
-    
-    $(selector).on("change", function() {
-        
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {   
+    document.cookie = name+'=; Max-Age=-99999999;';  
+}
+
+function setupInputs(selector) {
+    document.querySelectorAll(selector).forEach((input) => {
+        input.value = getCookie(input.id);
+        input.addEventListener('change', textChanged);
     });
 }
 
+function textChanged() {
+   setCookie(this.id, this.value);
+}
+
+function enableClipboard(selector) {
+    let clipboard = new Clipboard(selector);
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
-    enableAll("#child .btn");
+    enableClipboard("#child .btn");
+    setupInputs("textarea");
 });
 
